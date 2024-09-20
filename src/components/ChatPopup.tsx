638e62f7e48f28e3c72bfa1c16640446d70ad8e9
TropicolX <jacobsbusayo@gmail.com>
2024-09-20 10:58:28 +0100

    fix: ui fixes

--- src/components/ChatPopup.tsx
+++ src/components/ChatPopup.tsx
@@ -22,7 +22,7 @@ const ChatPopup = ({ channel, isOpen, onClose }: ChatPopupProps) => {
       open={isOpen}
       onClose={onClose}
       title={<h2>In-call messages</h2>}
-      className="right-4 left-auto -bottom-2.5 h-[calc(100svh-6rem)] animate-none"
+      className="bottom-[5rem] right-4 left-auto h-[calc(100svh-6rem)] animate-none"
     >
       <div className="px-0 pb-3 pt-0 h-[calc(100%-66px)]">
         <Channel channel={channel}>
