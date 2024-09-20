2eada98031520e8cdef93dcde248d8fcc9233fd8
TropicolX <jacobsbusayo@gmail.com>
2024-09-20 12:53:03 +0100

    feat: code cleanup, ui fixes

--- src/components/ButtonWithIcon.tsx
+++ src/components/ButtonWithIcon.tsx
@@ -30,7 +30,7 @@ const ButtonWithIcon = ({
           : 'h-12 text-base pr-4 pl-3',
         variant === 'primary' && 'bg-primary hover:bg-hover-primary',
         variant === 'secondary' && 'bg-meet-blue hover:bg-[#0a4ec4]',
-        'inline-flex items-center justify-center text-center font-medium tracking-looser text-white hover:shadow transition-[border_.28s_cubic-bezier(.4,0,.2,1),box-shadow_.28s_cubic-bezier(.4,0,.2,1)] active:bg-deep-blue active:border-deep-blue'
+        'inline-flex items-center justify-center text-center font-medium tracking-looser text-white hover:shadow transition-[border_.28s_cubic-bezier(.4,0,.2,1),box-shadow_.28s_cubic-bezier(.4,0,.2,1)] active:bg-deep-blue active:border-deep-blue select-none'
       )}
     >
       <span className="flex item-center justify-center mr-2">{icon}</span>
